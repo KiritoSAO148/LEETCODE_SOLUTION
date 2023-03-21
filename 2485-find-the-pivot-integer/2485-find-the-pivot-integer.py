851864f021df9e36f4045f1d4eddaf4aa1b22281
quochuy148 <wuochuy02@gmail.com>
2023-03-22 00:16:28 +0700

    Time: 67 ms (45.25%), Space: 13.9 MB (49.46%) - LeetHub

---
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
@@ -0,0 +1,9 @@
+class Solution:
+    def pivotInteger(self, n: int) -> int:
+        f = [0] * (n + 1)
+        f[1] = 1
+        for i in range(2, n + 1): f[i] = f[i - 1] + i
+        ok = 0
+        for i in range(1, n + 1):
+            if f[i] == f[n] - f[i - 1]: return i
+        return -1
