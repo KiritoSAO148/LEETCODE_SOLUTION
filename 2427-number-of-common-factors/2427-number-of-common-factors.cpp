50f7e45a24d1d2b3536f2b7e0deb0ea432da259f
quochuy148 <wuochuy02@gmail.com>
2023-03-22 00:22:08 +0700

    Time: 0 ms (100.00%), Space: 5.9 MB (60.27%) - LeetHub

---
+++ 2427-number-of-common-factors/2427-number-of-common-factors.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    
+    int cnt(int n){
+        int c = 0;
+        for (int i = 1; i <= sqrt(n); ++i){
+            if (n % i == 0){
+                ++c;
+                if (i != n / i) ++c;
+            }
+        }
+        return c;
+    }
+    
+    int commonFactors(int a, int b) {
+        int g = __gcd(a, b);
+        return cnt(g);
+    }
+};
